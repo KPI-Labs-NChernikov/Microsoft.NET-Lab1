f472a0a7742a282daa7a4ab1c7a2b11658689b51
Chernikov Nikita <14nik20@gmail.com>
2022-02-24 03:22:54 +0200

    Removed unnecessary information

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Business;
 using ConsoleApp.Data;
 using Data;
-using Data.Models;
 using System;
 using System.Collections.Generic;
 
@@ -54,27 +53,6 @@ namespace ConsoleApp
                 Items = mainMenuItems
             };
             MainMenu.Print();
-
-            //
-            var result1 = Service.GetActors();
-            var result2 = Service.GetSpectacleCast(2);
-            var result3 = Service.GetActorsWithFilmography();
-            var result4 = Service.GetActorPerformances(6);
-            var result5 = Service.GetMoviesGroupedByGenres();
-            var result6 = Service.GetMoviesFromYear(2000);
-            var result7 = Service.GetTopMainRolesPopularActors(55);
-            var result8 = Service.FindActorByName("o");
-            var result9 = Service.GetUniversalGenres();
-            var result10 = Service.GetActorsDirectors();
-            var result11 = Service.GetActorsByGenre(4);
-            var result12 = Service.FindMoviesByDirectorName("sco");
-            var result13 = Service.FindPerformancesByName("an");
-            var result14 = Service.GetGenresStats();
-            var result15 = Service.FindSpectaclesByGenreNameStart("d");
-            var mov = new Movie();
-            var str = mov.GetType().Name;
-            //
-
             Console.ResetColor();
         }
     }
